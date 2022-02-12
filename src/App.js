ea469026eb66adbd92ca8dd89adc5f5c2928534e
MuzaffarJuraev <juraev_m2000@mail.ru>
2022-02-12 10:02:23 +0500

    categories add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import Home from "./page/home"
-import News from './page/news';
-import Post from './page/post';
+import Home from "./page/home";
+import News from "./page/news";
+import Post from "./page/post";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/news" element={ <News/> } />
-        <Route path="/post" element={ <Post/> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/post" element={<Post />} />
       </Routes>
     </div>
   );
